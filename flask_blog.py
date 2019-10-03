33ad01d4e036e95ff59a9997ec37652272101a11
Hemiptera1 <55675342+Hemiptera1@users.noreply.github.com>
2019-10-03 03:02:36 -0300

    progress (still vid3 25min)

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = '989f5f34012c953dd427138de864094e'
@@ -27,6 +29,19 @@ def home():
 @app.route("/about")
 def about():
 	return render_template("about.html", title = "About")
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f"Account successfully created for { form.username.data }!", "success")
+		return redirect(url_for("home"))
+	return render_template("register.html", title = "Register Page", form = form)
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template("login.html", title = "Login Page", form = form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
